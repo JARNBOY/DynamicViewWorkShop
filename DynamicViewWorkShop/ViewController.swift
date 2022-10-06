9e868335d2da011fa77ec4f26c6789737c7b267c
Papon <papon.s@lawyerplusgroup.com>
2022-10-06 21:23:52 +0700

    Initial Commit

---
+++ DynamicViewWorkShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicViewWorkShop
+//
+//  Created by Papon Supamongkonchai on 6/10/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
